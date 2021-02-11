800088a3a3e8bce2200764242e1969d60ffabda8
Ajeya Nayak <ajeyanayak@gmail.com>
2021-02-11 23:14:03 +0530

    vwebapp

--- source/main.py
+++ source/main.py
@@ -16,9 +16,20 @@ def get_home():
 def get_contactinfor():
     return render_template('contact.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def reg_user():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        age = request.form.get('age')
+        gender = request.form.get('gender')
+        city = request.form.get('city')
+        with open("userdata.csv", "a") as file_name:
+            file_name.write("{};{};{};{}".format(name, age, gender, city))
     return render_template('registration.html')
+
+@app.route('/list')
+def user_list():
+    return render_template('userdata.csv')
 
 if __name__ == '__main__':
     app.run(port= 5555, debug = True)
